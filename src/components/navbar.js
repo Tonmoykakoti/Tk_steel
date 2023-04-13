7a5c52fce1dbd98be180281c31d14e2641c2d65b
Tonmoykakoti <kakotitonmoy@gmail.com>
2023-04-13 23:00:09 +0530

    navbar change

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,32 +1,46 @@
 import React from "react";
-// import logo from "../images/logo.svg";
 import logoNew from "../TKSTEEL.png";
 import { pageLinks } from "../data";
-// import { socialLinks } from "../data";
+import { useState, useEffect, useRef } from "react";
 
 const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(false);
+  const linksRef = useRef(null);
+  const linksContainerRef = useRef(null);
+
   return (
     <nav className="navbar">
       <div className="nav-center">
         <div className="nav-header">
           <img src={logoNew} className="nav-logo" alt="backroads" />
-          <button type="button" className="nav-toggle" id="nav-toggle">
+          <button
+            type="button"
+            className="nav-toggle"
+            id="nav-toggle"
+            onClick={() => setShowLinks(!showLinks)}
+          >
             <i className="fas fa-bars"></i>
           </button>
         </div>
         {/* <!-- left this comment on purpose --> */}
-        <ul className="nav-links " id="nav-links">
-          {pageLinks.map((link) => {
-            const { id, href, text } = link;
-            return (
-              <li key={id}>
-                <a href={href} className="nav-link" rel="noreferrer">
-                  {text}
-                </a>
-              </li>
-            );
-          })}
-        </ul>
+        <div className="links-container " ref={linksContainerRef}>
+          <ul
+            className={`${showLinks ? "nav-links show-links" : "nav-links"}`}
+            id="nav-links"
+            ref={linksRef}
+          >
+            {pageLinks.map((link) => {
+              const { id, href, text } = link;
+              return (
+                <li key={id}>
+                  <a href={href} className="nav-link" rel="noreferrer">
+                    {text}
+                  </a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
 
         {/* <ul className="nav-icons">
           {socialLinks.map((links) => {
